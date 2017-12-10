592a65529f4c6ee9076179c528b9f26f28d749c8
Marcel Schwittlick <lzrm@mrzl.net>
2017-12-10 17:36:26 +0100

    refactors the main app a bit to do automatic login

--- run.py
+++ run.py
@@ -4,35 +4,44 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 
-from fabric.api import run, cd, env
+from fabric.api import run, cd, env, execute
 from fabric.context_managers import settings, hide
 from fabric.network import disconnect_all
 
+import configparser
 
 class FabricHelper(object):
     def __init__(self):
-        self._host = "marcel@lyrik.ddns.net"
-        self._key = "/home/mar/.ssh/id_rsa_marchi"
+        cfg = configparser.ConfigParser()
+        cfg.read("settings.ini")
 
-    def test(self):
-        returnStrings = []
+        self._host =cfg.get("lyrik", "host")
+        self._key = cfg.get("lyrik", "key")
+        self._home_dir = cfg.get("lyrik", "home_dir")
+        self._password = cfg.get("lyrik", "password")
+
         env.host_string = self._host
         env.key_filename = self._key
+        env.password = self._password
+        #with cd(self._home_dir):
 
-        home_dir = "/home/marcel/fabric"
-        with cd(home_dir):
-            returnStrings.append(run("uname -a"))
+    def uname(self):
+        return run("uname -a")
 
-            returnStrings.append(run("python -V"))
-            returnStrings.append(run("pip -V"))
+    def python_v(self):
+        return run("python -V")
 
+    def pip_v(self):
+        return run("pip -V")
+
+    def disconnect(self):
         disconnect_all()
-        return returnStrings
 
     def capture(self):
-        with settings(hide('running', 'commands', 'stdout', 'stderr')):
-            stdout = execute(self.parallel_exec, hosts=self.hosts)
-        return stdout
+        pass
+        #with settings(hide('running', 'commands', 'stdout', 'stderr')):
+        #    stdout = execute(self.parallel_exec, hosts=self.hosts)
+        #return stdout
 
 
 class Window(object):
@@ -42,19 +51,19 @@ class Window(object):
         self.root = tk.Tk()
         self.frame = tk.Frame(self.root, width=100, height=100)
 
-        self.topLabel = tk.Label(self.frame, text="だまれ")
+        self.topLabel = tk.Label(self.frame, text="黙れ—左", font=("Helvetica", 26))
         self.topLabel.pack()
 
-        self.e = tk.Entry(self.frame)
+        self.e = tk.Entry(self.frame, width=50)
         self.e.pack()
 
         self.e.delete(0, tk.END)
-        self.e.insert(0, "a default value")
+        self.e.insert(0, "/home/mar/Downloads/changi02.jpg")
 
-        self.runButton = tk.Button(self.frame, text="run", command=self.button_cb)
+        self.runButton = tk.Button(self.frame, text="走る (run)", command=self.button_cb)
         self.runButton.pack()
 
-        self.imageButton = tk.Button(self.frame, text="image", command=self.local_image_cb)
+        self.imageButton = tk.Button(self.frame, text="画像 (image)", command=self.local_image_cb)
         self.imageButton.pack()
 
         self.log = tk.Text(self.frame, height=10, width=120)
@@ -66,11 +75,16 @@ class Window(object):
 
     def button_cb(self):
         print(self.e.get())
-        logs = self.fabric.test()
-        for log in logs:
-            print(log)
-            self.log.insert(tk.END, log + "\n")
-            self.log.pack()
+        self.log.insert(tk.END, self.fabric.uname() + "\n")
+        self.log.pack()
+        tk.Tk.update(self.root)
+        self.log.insert(tk.END, self.fabric.python_v() + "\n")
+        self.log.pack()
+        tk.Tk.update(self.root)
+        self.log.insert(tk.END, self.fabric.pip_v() + "\n")
+        self.log.pack()
+        tk.Tk.update(self.root)
+
 
     def local_image_cb(self):
         fname = askopenfilename(
@@ -84,7 +98,7 @@ class Window(object):
             try:
                 self.e.delete(0, tk.END)
                 self.e.insert(0, fname)
-                self.e.pack(side="left")
+                self.e.pack()
             except:  # <- naked except is a bad idea
                 showerror("Open Source File", "Failed to read file\n'%s'" % fname)
             return
