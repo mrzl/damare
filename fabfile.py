e88766ad7d093506addcb8c5b34a146abbfc9672
Marcel Schwittlick <lzrm@mrzl.net>
2017-12-15 06:33:17 +0100

    automatic login for fab

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 
 env.hosts = ['marcel@lyrik.ddns.net']
 env.key_filename = '/home/mar/.ssh/id_rsa_marchi'
+env.password = open('pass', 'r').readlines()[0]
 
 
 def hello(name="hoho"):
@@ -21,5 +22,19 @@ def deploy():
         run("pip -V")
 
 
+def deploy_scheduler():
+    send('/home/mar/code/marcel/lyrik/scheduler/scheduler.py', '/opt/scheduler/')
+
+
+def deploy_scripts():
+    send('/home/mar/code/marcel/damare/scripts/train_r01.sh', '/opt/scheduler/jobs/')
+    send('/home/mar/code/marcel/damare/scripts/train_r02.sh', '/opt/scheduler/jobs/')
+    send('/home/mar/code/marcel/damare/scripts/train_r03.sh', '/opt/scheduler/jobs/')
+
+
 def send(local_path, remote_path):
-    put(local_path, remote_path, use_sudo=False)
+    put(local_path, remote_path, use_sudo=False, mirror_local_mode=True)
+
+
+def dl(remote_path, local_path):
+    get(remote_path, local_path)
